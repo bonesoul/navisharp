4ded6b3cd53aec78b41ce49723c99c97828196e7
shalafiraistlin@gmail.com <shalafiraistlin@gmail.com@c2ca0453-d4cf-6fab-270c-7a802823fb9a>
2010-05-19 18:38:54 +0000

    commiting initial patch source

---
+++ trunk/patcher/ceExplorerPatch/Patcher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ceExplorerPatch
+{
+    public static class Patcher
+    {
+        public static bool GotExplorerExe=false;
+        public static bool GotInitKey = false;
+        public static string Shell;
+
+        public static bool Check()
+        {
+            if (FileSystem.CheckFile("\\windows\\explorer.exe")) GotExplorerExe = true;
+            if (Registry.CheckKey(KEY_ROOT.HKEY_LOCAL_MACHINE, "init")) GotInitKey = true;
+            {
+                Shell = Registry.GetValue(KEY_ROOT.HKEY_LOCAL_MACHINE, "init", "Launch60").ToString();
+            }
+
+            if (GotExplorerExe && GotInitKey) return true;
+            else return false;
+        }
+
+        public static void Patch()
+        {
+            Registry.SetValue(KEY_ROOT.HKEY_LOCAL_MACHINE, "init", "Launch60", "explorer.exe");
+        }
+    }
+}
---
+++ trunk/patcher/ceExplorerPatch/Registry.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Win32;
+
+namespace ceExplorerPatch
+{
+    public enum KEY_ROOT
+    {
+        HKEY_CLASSES_ROOT,
+        HKEY_CURRENT_USER,
+        HKEY_LOCAL_MACHINE
+    }
+
+    public static class Registry
+    {
+        public static RegistryKey GetKey(KEY_ROOT root, string _key)
+        {
+            RegistryKey rootkey;
+            switch (root)
+            {
+                case KEY_ROOT.HKEY_CLASSES_ROOT:
+                    rootkey = Microsoft.Win32.Registry.ClassesRoot;
+                    break;
+                case KEY_ROOT.HKEY_CURRENT_USER:
+                    rootkey = Microsoft.Win32.Registry.CurrentUser;
+                    break;
+                case KEY_ROOT.HKEY_LOCAL_MACHINE:
+                    rootkey = Microsoft.Win32.Registry.LocalMachine;
+                    break;
+                default:
+                    rootkey = Microsoft.Win32.Registry.CurrentUser;
+                    break;
+            }
+            return rootkey.OpenSubKey(_key);
+        }
+        
+        public static bool CheckKey(KEY_ROOT root, string _key)
+        {
+            RegistryKey key=GetKey(root, _key);
+            if (key == null)
+                return false;
+            else
+                return true;
+        }
+
+        public static object GetValue(KEY_ROOT root, string _key, string _value)
+        {
+            RegistryKey key = GetKey(root, _key);
+            return key.GetValue(_value);
+        } 
+
+        public static void SetValue(KEY_ROOT root, string _key, string _name,object _value)
+        {
+            RegistryKey key = GetKey(root, _key);
+            key.SetValue(_name, _value);
+        }
+    }
+}
