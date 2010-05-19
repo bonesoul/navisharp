1f34eb704a00b6770cc11d4eef4f0382a6d53b6d
shalafiraistlin@gmail.com <shalafiraistlin@gmail.com@c2ca0453-d4cf-6fab-270c-7a802823fb9a>
2010-05-19 20:21:09 +0000

    releasing very first release: 0.0.1 alpha

--- trunk/patcher/ceExplorerPatch/Form1.cs
+++ trunk/patcher/ceExplorerPatch/Form1.cs
@@ -13,31 +13,25 @@ namespace ceExplorerPatch
     {
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            this.Width = Screen.PrimaryScreen.Bounds.Width;
+            this.Height = Screen.PrimaryScreen.Bounds.Height;
+            this.WindowState = FormWindowState.Maximized;
             CheckPatch();           
         }
 
         private void CheckPatch()
         {
             if (Patcher.Check()) cmdPatch.Enabled = true;
-            else cmdPatch.Enabled = true;
+            else cmdPatch.Enabled = false;
             listboxInfo.Items.Clear();
             listboxInfo.Items.Add("Got Explorer.exe: " + Patcher.GotExplorerExe);
             listboxInfo.Items.Add("Got Init Key: " + Patcher.GotInitKey);
             listboxInfo.Items.Add("Current Shell: " + Patcher.Shell);
-        }
-       
-        private void menuRunExplorer_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                FileSystem.Execute("\\windows\\explorer.exe");
-            }
-            catch (Exception exc) { }
         }
 
         private void cmdPatch_Click(object sender, EventArgs e)
